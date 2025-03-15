6639bf567fe564dd67c98312741dccf350143259
pequity <58497385+pequity@users.noreply.github.com>
2025-03-15 20:01:53 +0000

    auth finalized

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-const MainLayout = ({
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const MainLayout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (!isUserAuthenticated) {
+    redirect("/");
+  }
+
   return <div>{children}</div>;
 };
 
--- app/(landingPage)/layout.tsx
+++ app/(landingPage)/layout.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-const LandingPage = ({
+const LandingLayout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const { isAuthenticated } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+
+  if (isUserAuthenticated) {
+    redirect("/dashboard");
+  }
   return <div>{children}</div>;
 };
 
-export default LandingPage;
+export default LandingLayout;
